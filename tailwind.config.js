6c29e4372c44473214e15088bf334484c9ed189b
Nicolas Primo <nicolasprimo@gmail.com>
2022-09-16 18:29:25 -0300

    design feito

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      fraunces: ["Fraunces", "sans"],
+    },
+    screens: {
+      xss: "376",
+      xs: "576px",
+    },
   },
   plugins: [],
 };
